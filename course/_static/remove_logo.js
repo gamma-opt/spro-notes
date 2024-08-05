a3ad881cd13491c66a664624ccc8eb0c7963a4c4
Tunç Başar Köse <tunc.kose@aalto.fi>
2024-08-05 14:53:45 +0300

    Revert logo removal to CSS. It seems faster.

--- course/_static/remove_logo.js
+++
@@ -1,6 +0,0 @@
-function removeLogo() {
-    const logo = document.getElementsByClassName("btn__qelogo")[0];
-    logo.parentNode.removeChild(logo);
-}
-
-document.addEventListener('DOMContentLoaded', removeLogo, false);
